a96e2d20571509bfa175dfd4aa939a0414d52ad5
flydoing <1262868349@qq.com>
2026-01-21 21:25:05 +0800

    feat: add post resource

--- project_demo/demo-01/src/app.module.ts
+++ project_demo/demo-01/src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   /**
    * imports - 导入功能模块
    * UserModule 已通过 nest g resource user 创建
    */
-  imports: [UserModule],
+  imports: [UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
